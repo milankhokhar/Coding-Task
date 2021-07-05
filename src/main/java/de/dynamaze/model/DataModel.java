22f98b47e1791c6296f5512ed89a0eb358976659
Milan Khokhar <37777247+milankhokhar@users.noreply.github.com>
2021-07-05 17:36:46 +0200

    Lombok annotation used for getter, setter and constructors

--- src/main/java/de/dynamaze/model/DataModel.java
+++ src/main/java/de/dynamaze/model/DataModel.java
@@ -1,60 +1,18 @@
 package de.dynamaze.model;
 
 
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class DataModel {
 
-
-    public String getHeadLine() {
-        return headLine;
-    }
-
-    public void setHeadLine(String headLine) {
-        this.headLine = headLine;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getHint() {
-        return hint;
-    }
-
-    public void setHint(String hint) {
-        this.hint = hint;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public DataModel(String headLine, String description, String hint, String link, Date date) {
-        this.headLine = headLine;
-        this.description = description;
-        this.hint = hint;
-        this.link = link;
-        this.date = date;
-    }
 
     private String headLine;
     private String description;
@@ -64,10 +22,6 @@ public class DataModel {
     @DateTimeFormat(pattern = "yyyy-mm-dd")
     private Date date;
 
-    public DataModel(){}
-
-
-
-
+    
 
 }
