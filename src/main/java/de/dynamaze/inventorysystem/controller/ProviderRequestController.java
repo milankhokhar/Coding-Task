d150fd2e781bef06576a624583e1bee99bf1a426
Milan Khokhar <37777247+milankhokhar@users.noreply.github.com>
2021-07-13 15:13:00 +0200

    try to implement using interface

--- src/main/java/de/dynamaze/inventorysystem/controller/ProviderRequestController.java
+++ src/main/java/de/dynamaze/inventorysystem/controller/ProviderRequestController.java
@@ -4,6 +4,7 @@ package de.dynamaze.inventorysystem.controller;
 import de.dynamaze.inventorysystem.descriptionModel.Description;
 import de.dynamaze.inventorysystem.headLineModel.HeadLine;
 import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,45 +17,23 @@ import java.util.List;
 @EnableScheduling
 public class ProviderRequestController {
 
-//    ProviderRequestController providerActivityData=new ProviderRequestController();
-
-//    private List<String> headLine;
-
-    Description description=new Description();
-    HeadLine headLine=new HeadLine();
-//    private List<String> hint;
-//    private String link;
-//
-//    @DateTimeFormat(pattern = "yyyy-mm-dd")
-//    private Date date;
-
-
 
     @PostMapping("/provider")
-    private void getData(@RequestBody Description descriptionInfo, @RequestBody HeadLine headLineInfo){
+    private void getData(){
 
 
-
-//        providerActivityData.setHeadLine(providerActivity.getHeadLine());
-        description.setDescription(descriptionInfo.getDescription());
-        headLine.setHeadLine(headLineInfo.getHeadLine());
-//        providerActivityData.setHint(providerActivity.getHint());
-//        providerActivityData.setLink(providerActivity.getLink());
-        System.out.println(description+"headline"+headLine);
     }
 
-//    @PostMapping("/customer/date")
-//    private String date(@RequestBody ProviderRequestController providerActivity){
-//
-//        providerActivityData.setDate(providerActivity.getDate());
-//
-//        return "Date Selected";
-//    }
-//
-//    @GetMapping("/display")
-//    @Scheduled(cron = "* * * * 7 *")
-//    public ProviderRequestController displayData(ProviderRequestController providerActivityData){
-//        return providerActivityData;
-//
-//    }
+    @PostMapping("/customer/date")
+    private void date(){
+
+
+    }
+
+    @GetMapping("/display")
+ //   @Scheduled(cron = "* * * * 7 *")
+    public void displayData(){
+
+
+    }
 }
