5f2526d6db3e4d0a6d7389230e920b280e0f95c1
Milan Khokhar <37777247+milankhokhar@users.noreply.github.com>
2021-07-14 18:22:20 +0200

    use constructor to get values from provider

--- src/main/java/de/dynamaze/inventorysystem/model/LeisureActivity.java
+++ src/main/java/de/dynamaze/inventorysystem/model/LeisureActivity.java
@@ -1,10 +1,20 @@
 package de.dynamaze.inventorysystem.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LeisureActivity {
 
-    private Long provider_id;
+
     private String headLine;
     private List<String> description;
     private List<String> hint;
--- src/main/java/de/dynamaze/inventorysystem/model/LeisureProvider.java
+++ src/main/java/de/dynamaze/inventorysystem/model/LeisureProvider.java
@@ -1,7 +1,25 @@
 package de.dynamaze.inventorysystem.model;
 
-public class LeisureProvider {
+import lombok.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RestController
+public class LeisureProvider extends LeisureActivity {
+    
     private String url;
     private String provider_name;
+
+    @PostMapping("/provider")
+    private LeisureProvider getProviderData(@RequestBody LeisureProvider provider){
+
+       return provider;
+    }
 }
