57e24ad844537661ca899a10e5cdb77a2729d83e
Milan Khokhar <37777247+milankhokhar@users.noreply.github.com>
2021-08-10 21:04:47 +0200

    Added NoArgumentConstructor in ProviderAndActivity class

--- src/main/java/de/dynamaze/inventorysystem/model/ProviderAndActivity.java
+++ src/main/java/de/dynamaze/inventorysystem/model/ProviderAndActivity.java
@@ -1,11 +1,14 @@
 package de.dynamaze.inventorysystem.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.mail.MailProperties;
 
 import java.util.Map;
 
+import java.util.Map;
 
+@NoArgsConstructor
 public class ProviderAndActivity {
     private Map<LeisureProvider,LeisureActivity> providerWithActivity;
 
