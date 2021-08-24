81314fb20a9b1bd3cdd29973069b7606311e80d0
Milan Khokhar <37777247+milankhokhar@users.noreply.github.com>
2021-08-24 12:07:03 +0200

    Added Provider GetMapping with optional Id end point

--- src/main/java/de/dynamaze/inventorysystem/controller/LeisureActivityController.java
+++ src/main/java/de/dynamaze/inventorysystem/controller/LeisureActivityController.java
@@ -1,17 +1,14 @@
 package de.dynamaze.inventorysystem.controller;
 
 import de.dynamaze.inventorysystem.commands.CreateActivity;
-import de.dynamaze.inventorysystem.commands.CreateProvider;
-import de.dynamaze.inventorysystem.model.LeisureActivity;
 import de.dynamaze.inventorysystem.model.LeisureProvider;
 import de.dynamaze.inventorysystem.model.ProviderAndActivity;
 import lombok.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Getter
 @AllArgsConstructor
@@ -30,10 +27,11 @@ public class LeisureActivityController {
 
     }
 
-    @PostMapping("provider")
+    @PostMapping("/provider")
     private void getProviderData(@RequestBody LeisureProvider provider){
         this.provider=provider;
     }
+
 
     @PostMapping("/customer/selectedDate")
     private String dateSelection(@RequestBody CreateActivity customerDate){
@@ -41,9 +39,13 @@ public class LeisureActivityController {
         return "Date Selected";
     }
 
-    @GetMapping("/leisureActivity")
-    @Scheduled(cron = "* * * * 7 *")
-    private ProviderAndActivity displayLeisureActivity(){
+    @GetMapping("/provider/id")
+    private LeisureProvider displayProviders(@RequestParam(required = false) UUID id){
+        return provider;
+    }
+
+    @GetMapping("/leisureActivity/id")
+    private ProviderAndActivity displayLeisureActivity(@RequestParam(required = false) UUID id){
         return providerAndActivity;
     }
 }
